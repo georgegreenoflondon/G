05c17c8ca06500a0b51f016e0f02d94c601b1b2a
George Green <george@thegreenfamily.net>
2014-11-23 12:32:03 +0000

    Initial Commit

---
+++ GInterpreter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GInterpreter
+//
+//  Created by George Green on 23/11/2014.
+//  Copyright (c) 2014 George Green. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
